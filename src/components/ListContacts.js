c1e131a321636e9a29372d0e51124618f551d4b0
Thounda Craig <thoundac94@gmail.com>
2023-03-01 20:14:33 -0500

    Passed props {data} from parent to child component

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,7 +1,13 @@
-// Define component to pass into UI
+// Define component to contacts data as a prop to map into an Ordered list
 const ListContacts = ({ contacts }) => {
-  console.log(contacts); // used just for testing - comment out for production build
-  return <ol className="contact-list"></ol>;
+  // console.log(contacts); // used just for testing - comment out for production build
+  return (
+    <ol className="contact-list">
+      {contacts.map((contact) => (
+        <li key={contact.id}>{contact.name}</li>
+      ))}
+    </ol>
+  );
 };
 
 // Set export on companent for use
